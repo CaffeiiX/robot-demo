14702dd3b2168407873167e9a7ac975b754a4f03
CaffeiiX <caichenxicode@foxmian.com>
2023-08-09 01:36:59 +0800

    test rachtext

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
+import '@chatui/core/es/styles/index.less';
+import '@chatui/core/dist/index.css';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { Layout } from 'antd';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import ChatSider from './page/ChatSider';
+import {useMessages} from '@chatui/core';
+const App: React.FC<{}> = () =>{
+  const [value, setValue] = useState('');
+  const changeRichText = (val: string) => {
+    setValue(val);
+  }
+  useEffect(() => {
+    setMessages({...messages, content: {...messages.content, html: value}});
+  }, [value])
+  const [messages, setMessages] = useState(
+    {
+      type: 'richText',
+      content: { html: '<p>Hello</p>' }
+    }
   );
+  return (
+    <>
+    <div style={{display: 'flex'}}>
+      <div style={{width: '60%', marginRight: '20px', height: '100vh'}}>
+        <ReactQuill theme='snow' value={value} onChange={changeRichText} style={{height: '100vh'}}/>
+      </div>
+      <div>
+        <ChatSider message={messages}/>
+      </div>
+    </div>
+    </>
+  )
 }
+
 
 export default App;
